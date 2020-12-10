4deaa894af6b0eea87f6d6c40f957080e072b968
laisflaviosilva <laisflavio.silva@gmail.com>
2020-12-10 18:28:40 -0300

    Criando a cobrinha

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-let canvas = document.getElementById("snake")
-let context = canvas.getContext("2d") //renderiza o desenho no canvas
+let canvas = document.getElementById("snake");
+let context = canvas.getContext("2d"); //renderiza o desenho no canvas
 let box = 32 // tamanho do box
-
-function criarBG(){
-    context.fillStyle = "lightgreen" //definindo a cor no background
-    context.fillRect(0, 0, 16 * box, 16 * box) //desenha o retangulo do jogo. Parametros Posição x e y, altura e largura
+let snake = [];
+snake[0]={
+    x: 8 * box,
+    y: 8 * box
 }
 
-criarBG()
+function criarBG(){
+    context.fillStyle = "lightgreen"; //definindo a cor no background
+    context.fillRect(0, 0, 16 * box, 16 * box); //desenha o retangulo do jogo. Parametros Posição x e y, altura e largura
+}
+
+function criarCobrinha(){
+    for(i=0; i< snake.length; i++){
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box)
+    }
+}
+
+criarBG();
+criarCobrinha();
